0f33cec0576905cbc4c1d3a3d04948c26ecb19a3
Yanis HADID <yanis5005@gmail.com>
2025-02-10 19:06:31 +0100

    suite first part doc 2

--- src/components/pictures.tsx
+++ src/components/pictures.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { counterSelector, getSelectedPicture, picturesSelector, selectPicture, closeModal } from '../reducer';
+import { Picture } from '../types/picture.type';
+import ModalPortal from './modal';
+import { Option, isSome } from 'fp-ts/Option'; // Import Option utilities
 
 const Container = styled.div`
   padding: 1rem;
@@ -11,14 +16,52 @@ const Container = styled.div`
 const Image = styled.img`
   margin: 10px;
   object-fit: contain;
-  transition: transform 1s;
-  max-width: fit-content;
+  transition: transform 0.3s ease-in-out;
+  max-width: 150px;
+  cursor: pointer;
   &:hover {
     transform: scale(1.2);
   }
 `;
+
 const Pictures = () => {
-  return null;
+  const dispatch = useDispatch();
+  const pictures = useSelector(picturesSelector); // Selector to get pictures
+  const counter = useSelector(counterSelector); // Selector to get counter value
+  const selectedPicture: Option<Picture> = useSelector(getSelectedPicture); // Selector to get selected picture
+
+  const handleClickPicture = (pic: Picture) => {
+    dispatch(selectPicture(pic)); // Dispatch action to select a picture
+  };
+
+  const handleCloseModal = () => {
+    dispatch(closeModal()); // Dispatch action to close the modal
+  };
+
+  return (
+    <>
+      {/* Display the thumbnails */}
+      <Container>
+        {pictures.slice(0, counter).map((pic, index) => (
+          <Image
+            key={index}
+            src={pic.previewFormat}
+            alt={`Cat by ${pic.author}`}
+            onClick={() => handleClickPicture(pic)}
+          />
+        ))}
+      </Container>
+
+      {/* Display the modal if a picture is selected */}
+      {isSome(selectedPicture) && (
+        <ModalPortal
+          largeFormat={selectedPicture.value.largeFormat}
+          author={selectedPicture.value.author}
+          close={handleCloseModal}
+        />
+      )}
+    </>
+  );
 };
 
 export default Pictures;
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,99 +1,101 @@
-import { Loop, Cmd, loop, liftState } from 'redux-loop';
+import { Cmd, Loop, liftState, loop } from 'redux-loop';
 import { compose } from 'redux';
 import { Actions } from './types/actions.type';
+import { Picture } from './types/picture.type';
+import { none, Option, some } from 'fp-ts/lib/Option';
+import fakeData from './fake-datas.json';
 
-// Define the State type
+
 export type State = {
-  counter: number;
-  pictures: string[];
-  selectedPicture: string | null;
-  loading: boolean;
-  fetchError: string | null;
-};
+  counter: number,
+  pictures: Picture[],
+  selectedPicture: Option<Picture>,
+}
 
-// Define the default state
 export const defaultState: State = {
-  counter: 0,
-  pictures: [],
-  selectedPicture: null,
-  loading: false,
-  fetchError: null,
-};
+  counter: 3,
+  pictures: fakeData,
+  selectedPicture: none,
+}
 
-// Action types
 type Increment = { type: 'INCREMENT' };
 type Decrement = { type: 'DECREMENT' };
-type SelectPicture = { type: 'SELECT_PICTURE'; payload: string };
-type CloseModal = { type: 'CLOSE_MODAL' };
 type FetchCatsRequest = { type: 'FETCH_CATS_REQUEST' };
-type FetchCatsCommit = { type: 'FETCH_CATS_COMMIT'; payload: string[] };
-type FetchCatsRollback = { type: 'FETCH_CATS_ROLLBACK'; payload: string };
+type FetchCatsCommit = { type: 'FETCH_CATS_COMMIT'; pictures: Picture[] };
+type FetchCatsRollback = { type: 'FETCH_CATS_ROLLBACK' };
+type SelectPicture = { type: 'SELECT_PICTURE'; picture: Picture };
+type CloseModal = { type: 'CLOSE_MODAL' };
 
 type Actions =
   | Increment
   | Decrement
-  | SelectPicture
-  | CloseModal
   | FetchCatsRequest
   | FetchCatsCommit
-  | FetchCatsRollback;
+  | FetchCatsRollback
+  | SelectPicture
+  | CloseModal;
 
-// Action creators
-export const increment = (): Increment => ({ type: 'INCREMENT' });
-export const decrement = (): Decrement => ({ type: 'DECREMENT' });
-export const selectPicture = (picture: string): SelectPicture => ({ type: 'SELECT_PICTURE', payload: picture });
+const increment = (): Increment => ({ type: 'INCREMENT' });
+const decrement = (): Decrement => ({ type: 'DECREMENT' });
+const fetchCatsRequest = (): FetchCatsRequest => ({ type: 'FETCH_CATS_REQUEST' });
+const fetchCatsCommit = (pictures: Picture[]): FetchCatsCommit => ({ type: 'FETCH_CATS_COMMIT', pictures });
+const fetchCatsRollback = (): FetchCatsRollback => ({ type: 'FETCH_CATS_ROLLBACK' });
+export const selectPicture = (picture: Picture): SelectPicture => ({ type: 'SELECT_PICTURE', picture });
 export const closeModal = (): CloseModal => ({ type: 'CLOSE_MODAL' });
-export const fetchCatsRequest = (): FetchCatsRequest => ({ type: 'FETCH_CATS_REQUEST' });
-export const fetchCatsCommit = (pictures: string[]): FetchCatsCommit => ({ type: 'FETCH_CATS_COMMIT', payload: pictures });
-export const fetchCatsRollback = (error: string): FetchCatsRollback => ({ type: 'FETCH_CATS_ROLLBACK', payload: error });
 
-// Reducer implementation
+
+
+const fetchPicturesFromAPI = async (counter: number): Promise<Picture[]> => {
+  const API_KEY = 'u_uomcf891am';
+  const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&per_page=${3}&q=cat`);
+  const data = await response.json();
+  
+  return data.hits.map((hit: any) => ({
+    previewFormat: hit.previewURL,
+    webformatFormat: hit.webformatURL,
+    author: hit.user,
+    largeFormat: hit.largeImageURL,
+  }));
+};
+
 export const reducer = (state: State | undefined, action: Actions): State | Loop<State> => {
-  if (!state) return defaultState; // Mandatory by Redux
-
+  if (!state) return defaultState;
   switch (action.type) {
     case 'INCREMENT':
-      return { ...state, counter: state.counter + 1 };
-
+      return { ...state, counter: state.counter + 1};
     case 'DECREMENT':
-      return { ...state, counter: Math.max(0, state.counter - 1) }; // Ensure counter doesn't go below 0
-
+      return { ...state, counter: Math.max(3, state.counter - 1) };
+    case 'FETCH_CATS_COMMIT':
+      return { ...state, pictures: action.pictures };
     case 'SELECT_PICTURE':
-      return { ...state, selectedPicture: action.payload };
-
+      return { ...state, selectedPicture: some(action.picture) };
     case 'CLOSE_MODAL':
-      return { ...state, selectedPicture: null };
-
+      return { ...state, selectedPicture: none };
     case 'FETCH_CATS_REQUEST':
       return loop(
-        { ...state, loading: true },
-        Cmd.run(fetchCatsApi, {
-          successActionCreator: (pictures) => fetchCatsCommit(pictures),
-          failActionCreator: (error: { message: string; }) => fetchCatsRollback(error.message),
+        state,
+        Cmd.run(fetchPicturesFromAPI, {
+          args: [state.counter],
+          successActionCreator: fetchCatsCommit,
+          failActionCreator: fetchCatsRollback,
         })
       );
-
-    case 'FETCH_CATS_COMMIT':
-      return { ...state, pictures: action.payload, loading: false, fetchError: null };
-
+    
     case 'FETCH_CATS_ROLLBACK':
-      return { ...state, loading: false, fetchError: action.payload };
-
-    default:
-      return state;
+      return { ...state, pictures: [] };
   }
 };
 
-// Selectors
-export const counterSelector = (state: State) => state.counter;
-export const picturesSelector = (state: State) => state.pictures;
-export const getSelectedPicture = (state: State) => state.selectedPicture;
-
-// Helper function to simulate API call (replace this with actual API logic)
-const fetchCatsApi = async (): Promise<string[]> => {
-  return new Promise((resolve) =>
-    setTimeout(() => resolve(['cat1.jpg', 'cat2.jpg', 'cat3.jpg']), 1000)
-  );
+export const counterSelector = (state: State) => {
+  return state.counter;
 };
+export const picturesSelector = (state: State) => {
+  return state.pictures;
+};
+export const getSelectedPicture = (state: State) => {
+  return state.selectedPicture;
+};
+
+
 
 export default compose(liftState, reducer);
